95eec8db490c240a4a114ded88a0c1bdd67e1408
Adrian Zalewski <aazalewski@hotmail.com>
2017-01-19 00:30:31 +0100

    User model: lint

--- models/user.js
+++ models/user.js
@@ -1,6 +1,5 @@
-'use strict';
 module.exports = function(sequelize, DataTypes) {
-  var User = sequelize.define('User', {
+  const User = sequelize.define('User', {
     username: DataTypes.STRING,
     password: DataTypes.STRING
   }, {
