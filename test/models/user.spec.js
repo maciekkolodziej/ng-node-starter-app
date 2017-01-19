94a27403b71e2a9ee93af9e88d76a1898be57525
Adrian Zalewski <aazalewski@hotmail.com>
2017-01-19 10:48:32 +0100

    User model: adding required data in placeholder test

--- test/models/user.spec.js
+++ test/models/user.spec.js
@@ -4,8 +4,10 @@ require('should');
 
 describe('user model', () => {
   it('should exist', (done) => {
-    User.create({})
-      .then((user) => {
+    User.create({
+      username: 'JohnDoe',
+      password: 'password',
+    }).then((user) => {
         user.should.be.ok;
         done();
       });
