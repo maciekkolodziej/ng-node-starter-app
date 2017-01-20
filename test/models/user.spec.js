a92306c88c5e1e41a53289de52463479a88858fc
Justyna Rachowicz <mrowa44@gmail.com>
2017-01-20 14:01:24 +0100

    Add specs for user creation

--- test/models/user.spec.js
+++ test/models/user.spec.js
@@ -1,15 +1,41 @@
+const bcrypt = require('bcrypt');
+
 const { User } = require('../../models');
+
+const SALT_ROUNDS = 10;
+const correctUser = {
+  username: 'JohnDoe',
+  password: 'password',
+};
 
 require('should');
 
 describe('user model', () => {
   it('should exist', (done) => {
-    User.create({
-      username: 'JohnDoe',
-      password: 'password',
-    }).then((user) => {
-        user.should.be.ok;
-        done();
-      });
+    User.create(correctUser).then((user) => {
+      user.should.be.ok;
+      done();
+    });
+  });
+
+  it('hashes password before create', done => {
+    User.create(correctUser)
+      .then(user => bcrypt.compare(correctUser.password, user.password))
+      .then(isCorrect => {
+        if (isCorrect) { return done(); }
+        throw new Error('Password is not hashed');
+      })
+      .catch(done);
+  });
+
+  it('doesn\'t update password if it wasn\'t changed', done => {
+    User.create(correctUser)
+      .then(user => user.update({ username: 'JaneDoe' }))
+      .then(updatedUser => bcrypt.compare(correctUser.password, updatedUser.password))
+      .then(isCorrect => {
+        if (isCorrect) { return done(); }
+        throw new Error('Password was updated');
+      })
+      .catch(done);
   });
 });
