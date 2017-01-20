64c7a21e10bc0287aa3bc90eff971ece26877060
Justyna Rachowicz <mrowa44@gmail.com>
2017-01-20 15:31:59 +0100

    Add test for successful password update

--- test/models/user.spec.js
+++ test/models/user.spec.js
@@ -2,7 +2,6 @@ const bcrypt = require('bcrypt');
 
 const { User } = require('../../models');
 
-const SALT_ROUNDS = 10;
 const correctUser = {
   username: 'JohnDoe',
   password: 'password',
@@ -21,20 +20,34 @@ describe('user model', () => {
   it('hashes password before create', done => {
     User.create(correctUser)
       .then(user => bcrypt.compare(correctUser.password, user.password))
-      .then(isCorrect => {
-        if (isCorrect) { return done(); }
-        throw new Error('Password is not hashed');
+      .then(isCorrectPassword => {
+        isCorrectPassword.should.be.ok();
+        done();
       })
       .catch(done);
+  });
+
+  it('successfully updates password', done => {
+    const fakePassword = 'secret';
+
+    User.create(correctUser)
+      .then(user => user.update({ password: fakePassword }))
+      .then(updatedUser => bcrypt.compare(fakePassword, updatedUser.password))
+      .then(isCorrectPassword => {
+        isCorrectPassword.should.be.ok();
+        done();
+      })
+      .catch(done);
+
   });
 
   it('doesn\'t update password if it wasn\'t changed', done => {
     User.create(correctUser)
       .then(user => user.update({ username: 'JaneDoe' }))
       .then(updatedUser => bcrypt.compare(correctUser.password, updatedUser.password))
-      .then(isCorrect => {
-        if (isCorrect) { return done(); }
-        throw new Error('Password was updated');
+      .then(isCorrectPassword => {
+        isCorrectPassword.should.be.ok();
+        done();
       })
       .catch(done);
   });
