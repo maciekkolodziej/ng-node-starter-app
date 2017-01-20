b6bbccd186ab014962b9ce824f1425407a063063
Justyna Rachowicz <mrowa44@gmail.com>
2017-01-20 15:33:06 +0100

    Make usernames unique and restore previous export style

--- api/controllers/users.js
+++ api/controllers/users.js
@@ -1,11 +1,9 @@
 const { User } = require('../../models');
 
-function register(req, res) {
-  User.create(req.body)
-    .then(user => res.status(201).send(user))
-    .catch(error => res.status(500).send(error));
-}
-
 module.exports = {
-  userRegister: register,
+  userRegister(req, res) {
+    User.create(req.body)
+      .then(user => res.status(201).send(user))
+      .catch(error => res.status(500).send(error));
+  }
 };
