a45ecfa9efe783418d71624bc6fed98c6c139f2d
Justyna Rachowicz <mrowa44@gmail.com>
2017-01-20 21:51:12 +0100

    Throw 409 code for username duplicate

--- api/controllers/users.js
+++ api/controllers/users.js
@@ -4,6 +4,11 @@ module.exports = {
   userRegister(req, res) {
     User.create(req.body)
       .then(user => res.status(201).send(user))
-      .catch(error => res.status(500).send(error));
+      .catch(error => {
+        if ('username' in error.fields) {
+          return res.status(409).send(error);
+        }
+        return res.status(500).send(error);
+      });
   }
 };
