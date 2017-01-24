5941aa907b54fc74965e0152682952725f207aa2
Justyna Rachowicz <mrowa44@gmail.com>
2017-01-24 17:19:50 +0100

    Refactor tests to use before hooks

--- test/api/controllers/users.js
+++ test/api/controllers/users.js
@@ -14,7 +14,11 @@ const correctUser = {
 
 describe('controllers', () => {
   describe('users', () => {
-    beforeEach(() => User.destroy({ where: {} }));
+    beforeEach(done => {
+      User.destroy({ where: {} })
+        .then(() => User.create(correctUser))
+        .then(() => done());
+    });
 
     describe('POST /users', () => {
       describe('with missing password', () => {
@@ -32,32 +36,31 @@ describe('controllers', () => {
 
       describe('with existing username', () => {
         it('should respond with 409 Conflict code', done => {
-          User.create(correctUser)
-            .then(() => {
-              request(server)
-                .post('/api/users')
-                .send(correctUser)
-                .expect(409)
-                .end(error => {
-                  should.not.exist(error);
-                  done();
-                });
+          request(server)
+            .post('/api/users')
+            .send(correctUser)
+            .expect(409)
+            .end(error => {
+              should.not.exist(error);
+              done();
             });
         });
       });
 
       describe('with correct params', () => {
         it('should create and respond with new user account', (done) => {
+          const user = { username: 'user', password: 'pass' };
+
           request(server)
             .post('/api/users')
-            .send(correctUser)
+            .send(user)
             .expect(201)
             .end((err, res) => {
               should.not.exist(err);
 
-              res.body.username.should.eql(correctUser.username);
+              res.body.username.should.eql(user.username);
 
-              User.find({ where: { id: res.body.id } })
+              User.findById(res.body.id)
                 .then((registeredUser) => {
                   should.exist(registeredUser);
                   done();
@@ -71,41 +74,35 @@ describe('controllers', () => {
     describe('POST /users/login', () => {
       describe('with valid password', () => {
         it('responds with token', done => {
-          User.create(correctUser)
-            .then(() => {
-              request(server)
-                .post('/api/users/login')
-                .send(correctUser)
-                .expect(200)
-                .end((error, res) => {
-                  should.not.exist(error);
-                  const token = jwt.decode(res.body.token, JWT_TOKEN);
-                  const userId = token && token.id;
-                  const expires = token && Date.parse(token.expirationDate);
+          request(server)
+            .post('/api/users/login')
+            .send(correctUser)
+            .expect(200)
+            .end((error, res) => {
+              should.not.exist(error);
+              const token = jwt.decode(res.body.token, JWT_TOKEN);
+              const userId = token.id;
+              const expires = Date.parse(token.expirationDate);
 
-                  User.findById(userId)
-                    .then(user => {
-                      should.exist(user);
-                      expires.should.be.above(Date.now());
-                      done();
-                    });
+              User.findById(userId)
+                .then(user => {
+                  should.exist(user);
+                  expires.should.be.above(Date.now());
+                  done();
                 });
             });
         });
       });
 
-      describe('with invalid parameters', () => {
+      describe('with invalid password', () => {
         it('returns 401 code', done => {
-          User.create(correctUser)
-            .then(() => {
-              request(server)
-                .post('/api/users/login')
-                .send({ username: correctUser.username, password: 'wrong' })
-                .expect(401)
-                .end(error => {
-                  should.not.exist(error);
-                  done();
-                });
+          request(server)
+            .post('/api/users/login')
+            .send({ username: correctUser.username, password: 'wrong' })
+            .expect(401)
+            .end(error => {
+              should.not.exist(error);
+              done();
             });
         });
       });
@@ -113,37 +110,42 @@ describe('controllers', () => {
 
     describe('GET /users', () => {
       describe('with valid authentication token', () => {
-        it('responds with users', done => {
-          User.create(correctUser)
+        let token;
+        beforeEach(done => {
+          User.find({ where: { username: correctUser.username } })
             .then(user => {
-              const token = jwt.encode({
+              token = jwt.encode({
                 id: user.id,
                 expirationDate: new Date(Date.now() + EXPIRATION_TIME),
               }, JWT_TOKEN);
 
-              request(server)
-                .get('/api/users')
-                .set('Authorization', `Bearer ${token}`)
-                .expect(200)
-                .end((error, res) => {
-                  should.not.exist(error);
+              done();
+            });
+        });
 
-                  User.findAll()
-                    .then(users => {
-                      res.body.length.should.eql(users.length);
-                      done();
-                    });
+        it('responds with users', done => {
+          request(server)
+            .get('/api/users')
+            .set('Authorization', `Bearer ${token}`)
+            .expect(200)
+            .end((error, res) => {
+              should.not.exist(error);
+
+              User.findAll()
+                .then(users => {
+                  res.body.length.should.eql(users.length);
+                  done();
                 });
             });
         });
       });
 
       describe('with invalid authentication token', () => {
-        it('responds with 401 Unauthorized code', done => {
-          const token = jwt.encode({
-            id: Math.floor(Math.random() * 100),
-          }, JWT_TOKEN);
+        const token = jwt.encode({
+          id: Math.floor(Math.random() * 100),
+        }, JWT_TOKEN);
 
+        it('responds with 401 Unauthorized code', done => {
           request(server)
             .get('/api/users')
             .set('Authorization', `Bearer ${token}`)
