3a5c23eac0718f85717d0b5e9c9655b42d9ad735
Adrian Zalewski <aazalewski@hotmail.com>
2017-01-25 07:28:34 +0100

    User model spec: using synchronous bcrypt methods (because we can, in tests)

--- test/models/user.spec.js
+++ test/models/user.spec.js
@@ -11,12 +11,11 @@ require('should');
 
 describe('user model', () => {
   let user;
+
   beforeEach(() => {
     return User.destroy({ where: {} })
       .then(() => User.create(correctUser))
-      .then(createdUser => {
-        user = createdUser;
-      });
+      .then(createdUser => user = createdUser);
   });
 
   it('should exist', (done) => {
@@ -24,48 +23,47 @@ describe('user model', () => {
     done();
   });
 
-  it('doesn\'t allow duplicate username', done => {
-    User.create(correctUser)
-      .catch(error => {
-        error.fields.should.have.property('username');
-        done();
-      });
+  describe('validations', () => {
+    it('doesn\'t allow duplicate username', done => {
+      User.create(correctUser)
+        .catch(error => {
+          error.fields.should.have.property('username');
+          done();
+        });
+    });
   });
 
-  it('hashes password before create', done => {
-    bcrypt.compare(correctUser.password, user.password)
-      .then(isCorrectPassword => {
-        isCorrectPassword.should.be.ok;
-        done();
-      })
-      .catch(done);
-  });
+  describe('password hashing', () => {
+    it('hashes password when creating new user', () => {
+      bcrypt.compareSync(correctUser.password, user.password)
+        .should.be.ok;
+    });
 
-  it('successfully updates password', done => {
-    const fakePassword = 'secret';
+    it('successfully updates password', (done) => {
+      const newPassword = 'secret';
 
-    user.update({ password: fakePassword })
-      .then(updatedUser => bcrypt.compare(fakePassword, updatedUser.password))
-      .then(isCorrectPassword => {
-        isCorrectPassword.should.be.ok;
-        done();
-      })
-      .catch(done);
+      user.update({ password: newPassword })
+        .then(updatedUser => {
+          bcrypt.compareSync(newPassword, updatedUser.password)
+            .should.be.ok;
+          done();
+        })
+        .catch(done);
+    });
 
-  });
-
-  it('doesn\'t update password if it wasn\'t changed', done => {
-    user.update({ username: 'JaneDoe' })
-      .then(updatedUser => bcrypt.compare(correctUser.password, updatedUser.password))
-      .then(isCorrectPassword => {
-        isCorrectPassword.should.be.ok;
-        done();
-      })
-      .catch(done);
+    it('doesn\'t update password if it wasn\'t changed', (done) => {
+      user.update({ username: 'JaneDoe' })
+        .then(updatedUser => {
+          bcrypt.compareSync(correctUser.password, updatedUser.password)
+            .should.be.ok;
+          done();
+        })
+        .catch(done);
+    });
   });
 
   describe('isValidPassword method', () => {
-    it('returns true for valid password', done => {
+    it('returns true for valid password', (done) => {
       user.isValidPassword(correctUser.password)
         .then(isValid => {
           isValid.should.be.ok();
@@ -74,14 +72,13 @@ describe('user model', () => {
         .catch(done);
     });
 
-    it('returns false for invalid password', done => {
+    it('returns false for invalid password', (done) => {
       user.isValidPassword('invalid')
         .then(isValid => {
-          isValid.should.not.be.ok();
+          isValid.should.be.ok;
           done();
         })
         .catch(done);
     });
   });
-
 });
