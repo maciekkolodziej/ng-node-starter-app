6ebc49cbb50c15bf6510f1e31d839bd02b82a4db
Adrian Zalewski <aazalewski@hotmail.com>
2017-01-29 19:26:08 +0100

    .eslintrc: we need to be explicit about strict mode

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,16 @@
 module.exports = {
   extends: 'airbnb-base',
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: 'script',
+    ecmaFeatures: {
+      modules: false,
+    },
+  },
   rules: {
     'consistent-return': 0,
     'no-console': 1,
     'no-param-reassign': 0,
+    strict: [2, 'global'],
   },
 };
