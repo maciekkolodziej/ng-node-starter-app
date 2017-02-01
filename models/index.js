7fe526031d07acac0ee9c2e991f06f3427f51fb4
Adrian Zalewski <aazalewski@hotmail.com>
2017-02-01 19:55:39 +0100

    Index model file: removing redundant config require() call.

--- models/index.js
+++ models/index.js
@@ -8,8 +8,6 @@ var env       = process.env.NODE_ENV || 'development';
 var config    = require(__dirname + '/../config/config')[env];
 var db        = {};
 
-require('../config/config');
-
 if (config.use_env_variable) {
   var sequelize = new Sequelize(process.env[config.use_env_variable]);
 } else {
