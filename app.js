cca17f2da3045a2052be34d56f1026425a40c44d
Justyna Rachowicz <mrowa44@gmail.com>
2017-02-01 13:46:28 +0100

    Add dependencies

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const SwaggerExpress = require('swagger-express-mw');
 const passport = require('passport');
 const app = require('express')();
+const bodyParser = require('body-parser');
 
 const config = {
   appRoot: __dirname, // required config
@@ -10,11 +11,13 @@ const config = {
 
 require('./initializers/passport');
 
+
 SwaggerExpress.create(config, (err, swaggerExpress) => {
   if (err) { throw err; }
 
   // install middleware
   app.use(passport.initialize());
+  app.use(bodyParser.json());
   swaggerExpress.register(app);
 
   const port = process.env.PORT || 10010;
